d5cc02a0f387eebc56d7b400bb4f75c71951c89a
Felipe Magenis <133271953+FelipeMagenis@users.noreply.github.com>
2024-05-13 18:31:00 -0300

    Repository and Dto

--- src/main/java/br/com/felipemagenis/productsapirest/dtos/ProductRecordDto.java
+++ src/main/java/br/com/felipemagenis/productsapirest/dtos/ProductRecordDto.java
@@ -1,5 +1,9 @@
 package br.com.felipemagenis.productsapirest.dtos;
 
-public record ProductRecordDto() {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
+import java.math.BigDecimal;
+
+public record ProductRecordDto(@NotBlank String name, @NotNull BigDecimal value) {
 }
--- src/main/java/br/com/felipemagenis/productsapirest/repositories/ProductRepository.java
+++ src/main/java/br/com/felipemagenis/productsapirest/repositories/ProductRepository.java
@@ -1,5 +1,13 @@
 package br.com.felipemagenis.productsapirest.repositories;
 
-public interface ProductRepository {
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.felipemagenis.productsapirest.models.ProductModel;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
 
 }
